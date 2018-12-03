1f636faf26af64831d4d39fc8ae8842b82b7745d
daneparke <dapa8522@colorado.edu>
2018-12-03 09:42:43 -0700

    added dependecies

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3004
+const queries = require('./queries')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+
+const listener = () => console.log(`Listening on port ${port}`)
+app.listen(port, listener)
